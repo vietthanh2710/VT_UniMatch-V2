078ccaf597b4671133724ece49c31dfa9eaf41f2
vietthanh2710 <138222980+vietthanh2710@users.noreply.github.com>
2024-12-27 10:44:05 +0700

    Update transform.py

--- dataset/transform.py
+++ dataset/transform.py
@@ -32,7 +32,7 @@ def hflip(img, mask, p=0.5):
 def normalize(img, mask=None):
     img = transforms.Compose([
         transforms.ToTensor(),
-        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ])(img)
     if mask is not None:
         mask = torch.from_numpy(np.array(mask)).long()
