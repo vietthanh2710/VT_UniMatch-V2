32808fa7bb4fabb4cbd22894ea4131ae5ed7f965
vietthanh2710 <138222980+vietthanh2710@users.noreply.github.com>
2025-01-04 15:16:32 +0700

    Update supervised_weighted.py

--- supervised_weighted.py
+++ supervised_weighted.py
@@ -115,7 +115,7 @@ def main():
     cudnn.enabled = True
     cudnn.benchmark = True
 
-    model = smp.SegFormer(
+    model = smp.Segformer(
         encoder_name="mit_b2",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
         encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
         in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
