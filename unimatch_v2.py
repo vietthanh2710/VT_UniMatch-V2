45d086e7bd4d0f689b66ad116ee4c77e2b4f824a
vietthanh2710 <138222980+vietthanh2710@users.noreply.github.com>
2025-01-24 17:35:37 +0700

    Update unimatch_v2.py

--- unimatch_v2.py
+++ unimatch_v2.py
@@ -9,26 +9,115 @@ from torch import nn
 import torch.backends.cudnn as cudnn
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
+import torch.distributed as dist
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 import yaml
 
-from dataset.semi import SemiDataset
+# from dataset.semi import SemiDataset
+from dataset.supervised import SemiDataset, SemiDataset_Weight
 from model.semseg.dpt import DPT
-from supervised import evaluate
+# from supervised import evaluate
 from util.classes import CLASSES
 from util.ohem import ProbOhemCrossEntropy2d
-from util.utils import count_params, init_log, AverageMeter
+from segmentation_models_pytorch.losses import DiceLoss
+from util.utils import *
 from util.dist_helper import setup_distributed
 
+import warnings
+# Suppress specific warnings
+warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser(description='UniMatch V2: Pushing the Limit of Semi-Supervised Semantic Segmentation')
 parser.add_argument('--config', type=str, required=True)
 parser.add_argument('--labeled-id-path', type=str, required=True)
 parser.add_argument('--unlabeled-id-path', type=str, required=True)
+parser.add_argument('--pretrained-path', type=str, default=None)
 parser.add_argument('--save-path', type=str, required=True)
 parser.add_argument('--local_rank', '--local-rank', default=0, type=int)
 parser.add_argument('--port', default=None, type=int)
 
+
+Category = ['bic_pru','card','diw','invoice','passport']
+
+def weight_computation(sub_miou,local_rank):
+    n_samples = np.array([935,11894,5100,1738,3982])
+    inv_miou = (100 / sub_miou)**2
+    weight = inv_miou + 100/n_samples
+    weight = torch.tensor(weight, dtype=torch.float32, device = local_rank)
+    return weight
+
+def evaluate(model, loader, mode, cfg, multiplier=None):
+    model.eval()
+    assert mode in ['original', 'center_crop', 'sliding_window']
+    intersection_meter = MultiAverageMeter(5,2)
+    union_meter = MultiAverageMeter(5,2)
+
+    with torch.no_grad():
+        for img, mask, id in loader:
+            
+            img = img.cuda()
+                
+            if mode == 'sliding_window':
+                grid = cfg['crop_size']
+                b, _, h, w = img.shape
+                final = torch.zeros(b, 19, h, w).cuda()
+                
+                row = 0
+                while row < h:
+                    col = 0
+                    while col < w:
+                        pred = model(img[:, :, row: row + grid, col: col + grid])
+                        final[:, :, row: row + grid, col: col + grid] += pred.softmax(dim=1)
+                        if col == w - grid:
+                            break
+                        col = min(col + int(grid * 2 / 3), w - grid)
+                    if row == h - grid:
+                        break
+                    row = min(row + int(grid * 2 / 3), h - grid)
+                    
+                pred = final
+            
+            else:
+                assert mode == 'original'
+                
+                if multiplier is not None:
+                    ori_h, ori_w = img.shape[-2:]
+                    if multiplier == 512:
+                        new_h, new_w = 512, 512
+                    else:
+                        new_h, new_w = int(ori_h / multiplier + 0.5) * multiplier, int(ori_w / multiplier + 0.5) * multiplier
+                    # img = F.interpolate(img, (new_h, new_w), mode='bilinear', align_corners=True)
+                    img = F.interpolate(img, (308, 308), mode='bilinear', align_corners=True)
+                pred = model(img)
+            
+                if multiplier is not None:
+                    pred = F.interpolate(pred, (ori_h, ori_w), mode='bilinear', align_corners=True)
+
+            pred = pred.argmax(dim=1)
+            
+            intersection, union, target = \
+                intersectionAndUnion(pred.cpu().numpy(), mask.numpy(), cfg['nclass'], 255)
+            
+            reduced_intersection = torch.from_numpy(intersection).cuda()
+            reduced_union = torch.from_numpy(union).cuda()
+            reduced_target = torch.from_numpy(target).cuda()
+
+            dist.all_reduce(reduced_intersection)
+            dist.all_reduce(reduced_union)
+            dist.all_reduce(reduced_target)
+
+            intersection_meter.update(reduced_intersection.cpu().numpy(), id_path = ''.join(id))
+            union_meter.update(reduced_union.cpu().numpy(), id_path = ''.join(id))
+
+    iou_class = intersection_meter.sum / (union_meter.sum + 1e-10) * 100.0
+
+    n = np.array([104,1322,567,193,442])
+    mIoU_folder = np.mean(iou_class, axis = 1)
+    mIoU = np.sum(n*mIoU_folder) / 2631.
+
+    return mIoU, iou_class
 
 def main():
     args = parser.parse_args()
@@ -58,8 +147,7 @@ def main():
         'giant': {'encoder_size': 'giant', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
     }
     model = DPT(**{**model_configs[cfg['backbone'].split('_')[-1]], 'nclass': cfg['nclass']})
-    # state_dict = torch.load(f'./pretrained/{cfg["backbone"]}.pth')
-    state_dict = torch.load('/kaggle/input/dino-small-pretrained/dinov2_vits14_pretrain.pth')
+    state_dict = torch.load('/kaggle/input/doc_code/pytorch/default/1/1_Code/dinov2_vits14_pretrain.pth')
     model.backbone.load_state_dict(state_dict)
         
     if cfg['lock_backbone']:
@@ -92,7 +180,9 @@ def main():
         param.requires_grad = False
     
     if cfg['criterion']['name'] == 'CELoss':
-        criterion_l = nn.CrossEntropyLoss(**cfg['criterion']['kwargs']).cuda(local_rank)
+        # criterion_l = nn.CrossEntropyLoss(**cfg['criterion']['kwargs']).cuda(local_rank)
+        criterion_l1 = DiceLoss(mode='multiclass', **cfg['criterion']['kwargs']).cuda(local_rank)
+        criterion_l2 = nn.CrossEntropyLoss(**cfg['criterion']['kwargs']).cuda(local_rank)
     elif cfg['criterion']['name'] == 'OHEM':
         criterion_l = ProbOhemCrossEntropy2d(**cfg['criterion']['kwargs']).cuda(local_rank)
     else:
@@ -125,7 +215,7 @@ def main():
         valset, batch_size=1, pin_memory=True, num_workers=1, drop_last=False, sampler=valsampler
     )
     
-    total_iters = len(trainloader_u) * cfg['epochs']
+    total_iters = len(trainloader_u) * (cfg['epochs1']+cfg['epochs2'])
     previous_best, previous_best_ema = 0.0, 0.0
     best_epoch, best_epoch_ema = 0, 0
     epoch = -1
@@ -144,7 +234,11 @@ def main():
         if rank == 0:
             logger.info('************ Load from checkpoint at epoch %i\n' % epoch)
     
-    for epoch in range(epoch + 1, cfg['epochs']):
+    """
+    Train with equal distribution 
+    """
+
+    for epoch in range(epoch + 1, cfg['epochs1']):
         if rank == 0:
             logger.info('===========> Epoch: {:}, Previous best: {:.2f} @epoch-{:}, '
                         'EMA: {:.2f} @epoch-{:}'.format(epoch, previous_best, best_epoch, previous_best_ema, best_epoch_ema))
@@ -190,7 +284,8 @@ def main():
             conf_u_w_cutmixed2[cutmix_box2 == 1] = conf_u_w.flip(0)[cutmix_box2 == 1]
             ignore_mask_cutmixed2[cutmix_box2 == 1] = ignore_mask.flip(0)[cutmix_box2 == 1]
             
-            loss_x = criterion_l(pred_x, mask_x)
+            # loss_x = criterion_l(pred_x, mask_x)
+            loss_x = criterion_l1(pred_x, mask_x) + criterion_l2(pred_x, mask_x)
 
             loss_u_s1 = criterion_u(pred_u_s1, mask_u_w_cutmixed1)
             loss_u_s1 = loss_u_s1 * ((conf_u_w_cutmixed1 >= cfg['conf_thresh']) & (ignore_mask_cutmixed1 != 255))
@@ -215,7 +310,7 @@ def main():
             total_mask_ratio.update(mask_ratio.item())
 
             iters = epoch * len(trainloader_u) + i
-            lr = cfg['lr'] * (1 - iters / total_iters) ** 0.9
+            lr = cfg['lr'] * (1 - iters / total_iters) ** 0.95
             optimizer.param_groups[0]["lr"] = lr
             optimizer.param_groups[1]["lr"] = lr * cfg['lr_multi']
             
@@ -242,16 +337,18 @@ def main():
         mIoU_ema, iou_class_ema = evaluate(model_ema, valloader, eval_mode, cfg, multiplier=14)
         
         if rank == 0:
-            for (cls_idx, iou) in enumerate(iou_class):
-                logger.info('***** Evaluation ***** >>>> Class [{:} {:}] IoU: {:.2f}, '
-                            'EMA: {:.2f}'.format(cls_idx, CLASSES[cfg['dataset']][cls_idx], iou, iou_class_ema[cls_idx]))
+            for cls_idx, (iou, iou_ema) in enumerate(zip(iou_class, iou_class_ema)):
+                logger.info(f'{Category[cls_idx]:<8}')
+                logger.info(f'\tIOU: {iou[0]:.2f} | {iou[1]:.2f} --> Mean: {np.mean(iou):.2f}%')
+                logger.info(f'\tEMA: {iou_ema[0]:.2f} | {iou_ema[1]:.2f} --> Mean: {np.mean(iou_ema):.2f}%')
+
             logger.info('***** Evaluation {} ***** >>>> MeanIoU: {:.2f}, EMA: {:.2f}\n'.format(eval_mode, mIoU, mIoU_ema))
             
             writer.add_scalar('eval/mIoU', mIoU, epoch)
             writer.add_scalar('eval/mIoU_ema', mIoU_ema, epoch)
-            for i, iou in enumerate(iou_class):
-                writer.add_scalar('eval/%s_IoU' % (CLASSES[cfg['dataset']][i]), iou, epoch)
-                writer.add_scalar('eval/%s_IoU_ema' % (CLASSES[cfg['dataset']][i]), iou_class_ema[i], epoch)
+            for i, (iou, iou_ema) in enumerate(zip(iou_class, iou_class_ema)):
+                writer.add_scalar('eval/%s_IoU' % (Category[i]), np.mean(iou), epoch)
+                writer.add_scalar('eval/%s_IoU' % (Category[i]), np.mean(iou_ema), epoch)
 
         is_best = mIoU >= previous_best
         
@@ -275,8 +372,158 @@ def main():
             }
             torch.save(checkpoint, os.path.join(args.save_path, 'latest.pth'))
             if is_best:
-                torch.save(checkpoint, os.path.join(args.save_path, 'best.pth'))
+                torch.save(checkpoint, os.path.join(args.save_path, 'best1.pth'))
+    
+    """
+    Reweight via iou score and n_sample
+    """
 
+    sub_miou = np.mean(iou_class, axis = 1)
+    folder_weight = weight_computation(sub_miou, local_rank)
+    print(folder_weight)
+    logger.info(folder_weight)
 
+    trainset_l_new = SemiDataset_Weight(
+        cfg['dataset'], cfg['data_root'], 'train_l', cfg['crop_size'], args.labeled_id_path, nsample=len(trainset_u.ids), weight = folder_weight
+    )
+    trainsampler_l_new = torch.utils.data.WeightedRandomSampler(weights=trainset_l_new.sample_weight, num_samples=len(trainset_l_new), replacement=True)
+    trainloader_l_new = DataLoader(
+        trainset_l_new, batch_size=cfg['batch_size'], pin_memory=True, num_workers=4, drop_last=True, sampler=trainsampler_l_new
+    )
+
+    for epoch in range(epoch + 1, cfg['epochs1']+ cfg['epochs2']):
+        if rank == 0:
+            logger.info('===========> Epoch: {:}, Previous best: {:.2f} @epoch-{:}, '
+                        'EMA: {:.2f} @epoch-{:}'.format(epoch, previous_best, best_epoch, previous_best_ema, best_epoch_ema))
+        
+        total_loss  = AverageMeter()
+        total_loss_x = AverageMeter()
+        total_loss_s = AverageMeter()
+        total_mask_ratio = AverageMeter()
+
+        trainloader_u.sampler.set_epoch(epoch)
+
+        loader = zip(trainloader_l_new, trainloader_u)
+        
+        model.train()
+
+        for i, ((img_x, mask_x),
+                (img_u_w, img_u_s1, img_u_s2, ignore_mask, cutmix_box1, cutmix_box2)) in enumerate(loader):
+            
+            img_x, mask_x = img_x.cuda(), mask_x.cuda()
+            img_u_w, img_u_s1, img_u_s2 = img_u_w.cuda(), img_u_s1.cuda(), img_u_s2.cuda()
+            ignore_mask, cutmix_box1, cutmix_box2 = ignore_mask.cuda(), cutmix_box1.cuda(), cutmix_box2.cuda()
+            
+            with torch.no_grad():
+                pred_u_w = model_ema(img_u_w).detach()
+                conf_u_w = pred_u_w.softmax(dim=1).max(dim=1)[0]
+                mask_u_w = pred_u_w.argmax(dim=1)
+            
+            img_u_s1[cutmix_box1.unsqueeze(1).expand(img_u_s1.shape) == 1] = img_u_s1.flip(0)[cutmix_box1.unsqueeze(1).expand(img_u_s1.shape) == 1]
+            img_u_s2[cutmix_box2.unsqueeze(1).expand(img_u_s2.shape) == 1] = img_u_s2.flip(0)[cutmix_box2.unsqueeze(1).expand(img_u_s2.shape) == 1]
+            
+            pred_x = model(img_x)
+            pred_u_s1, pred_u_s2 = model(torch.cat((img_u_s1, img_u_s2)), comp_drop=True).chunk(2)
+            
+            mask_u_w_cutmixed1, conf_u_w_cutmixed1, ignore_mask_cutmixed1 = mask_u_w.clone(), conf_u_w.clone(), ignore_mask.clone()
+            mask_u_w_cutmixed2, conf_u_w_cutmixed2, ignore_mask_cutmixed2 = mask_u_w.clone(), conf_u_w.clone(), ignore_mask.clone()
+
+            mask_u_w_cutmixed1[cutmix_box1 == 1] = mask_u_w.flip(0)[cutmix_box1 == 1]
+            conf_u_w_cutmixed1[cutmix_box1 == 1] = conf_u_w.flip(0)[cutmix_box1 == 1]
+            ignore_mask_cutmixed1[cutmix_box1 == 1] = ignore_mask.flip(0)[cutmix_box1 == 1]
+            
+            mask_u_w_cutmixed2[cutmix_box2 == 1] = mask_u_w.flip(0)[cutmix_box2 == 1]
+            conf_u_w_cutmixed2[cutmix_box2 == 1] = conf_u_w.flip(0)[cutmix_box2 == 1]
+            ignore_mask_cutmixed2[cutmix_box2 == 1] = ignore_mask.flip(0)[cutmix_box2 == 1]
+            
+            # loss_x = criterion_l(pred_x, mask_x)
+            loss_x = criterion_l1(pred_x, mask_x) + criterion_l2(pred_x, mask_x)
+
+            loss_u_s1 = criterion_u(pred_u_s1, mask_u_w_cutmixed1)
+            loss_u_s1 = loss_u_s1 * ((conf_u_w_cutmixed1 >= cfg['conf_thresh']) & (ignore_mask_cutmixed1 != 255))
+            loss_u_s1 = loss_u_s1.sum() / (ignore_mask_cutmixed1 != 255).sum().item()
+            
+            loss_u_s2 = criterion_u(pred_u_s2, mask_u_w_cutmixed2)
+            loss_u_s2 = loss_u_s2 * ((conf_u_w_cutmixed2 >= cfg['conf_thresh']) & (ignore_mask_cutmixed2 != 255))
+            loss_u_s2 = loss_u_s2.sum() / (ignore_mask_cutmixed2 != 255).sum().item()
+            
+            loss_u_s = (loss_u_s1 + loss_u_s2) / 2.0
+            
+            loss = (loss_x + loss_u_s) / 2.0
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss.update(loss.item())
+            total_loss_x.update(loss_x.item())
+            total_loss_s.update(loss_u_s.item())
+            mask_ratio = ((conf_u_w >= cfg['conf_thresh']) & (ignore_mask != 255)).sum().item() / (ignore_mask != 255).sum()
+            total_mask_ratio.update(mask_ratio.item())
+
+            iters = epoch * len(trainloader_u) + i
+            lr = cfg['lr'] * (1 - iters / total_iters) ** 0.95
+            optimizer.param_groups[0]["lr"] = lr
+            optimizer.param_groups[1]["lr"] = lr * cfg['lr_multi']
+            
+            ema_ratio = min(1 - 1 / (iters + 1), 0.996)
+            
+            for param, param_ema in zip(model.parameters(), model_ema.parameters()):
+                param_ema.copy_(param_ema * ema_ratio + param.detach() * (1 - ema_ratio))
+            for buffer, buffer_ema in zip(model.buffers(), model_ema.buffers()):
+                buffer_ema.copy_(buffer_ema * ema_ratio + buffer.detach() * (1 - ema_ratio))
+            
+            if rank == 0:
+                writer.add_scalar('train/loss_all', loss.item(), iters)
+                writer.add_scalar('train/loss_x', loss_x.item(), iters)
+                writer.add_scalar('train/loss_s', loss_u_s.item(), iters)
+                writer.add_scalar('train/mask_ratio', mask_ratio, iters)
+
+            if (i % (len(trainloader_u) // 8) == 0) and (rank == 0):
+                logger.info('Iters: {:}, LR: {:.7f}, Total loss: {:.3f}, Loss x: {:.3f}, Loss s: {:.3f}, Mask ratio: '
+                            '{:.3f}'.format(i, optimizer.param_groups[0]['lr'], total_loss.avg, total_loss_x.avg, 
+                                            total_loss_s.avg, total_mask_ratio.avg))
+        
+        eval_mode = 'sliding_window' if cfg['dataset'] == 'cityscapes' else 'original'
+        mIoU, iou_class = evaluate(model, valloader, eval_mode, cfg, multiplier=14)
+        mIoU_ema, iou_class_ema = evaluate(model_ema, valloader, eval_mode, cfg, multiplier=14)
+        
+        if rank == 0:
+            for cls_idx, (iou, iou_ema) in enumerate(zip(iou_class, iou_class_ema)):
+                logger.info(f'{Category[cls_idx]:<8}')
+                logger.info(f'\tIOU: {iou[0]:.2f} | {iou[1]:.2f} --> Mean: {np.mean(iou):.2f}%')
+                logger.info(f'\tEMA: {iou_ema[0]:.2f} | {iou_ema[1]:.2f} --> Mean: {np.mean(iou_ema):.2f}%')
+
+            logger.info('***** Evaluation {} ***** >>>> MeanIoU: {:.2f}, EMA: {:.2f}\n'.format(eval_mode, mIoU, mIoU_ema))
+            
+            writer.add_scalar('eval/mIoU', mIoU, epoch)
+            writer.add_scalar('eval/mIoU_ema', mIoU_ema, epoch)
+            for i, (iou, iou_ema) in enumerate(zip(iou_class, iou_class_ema)):
+                writer.add_scalar('eval/%s_IoU' % (Category[i]), np.mean(iou), epoch)
+                writer.add_scalar('eval/%s_IoU' % (Category[i]), np.mean(iou_ema), epoch)
+
+        is_best = mIoU >= previous_best
+        
+        previous_best = max(mIoU, previous_best)
+        previous_best_ema = max(mIoU_ema, previous_best_ema)
+        if mIoU == previous_best:
+            best_epoch = epoch
+        if mIoU_ema == previous_best_ema:
+            best_epoch_ema = epoch
+        
+        if rank == 0:
+            checkpoint = {
+                'model': model.state_dict(),
+                'model_ema': model_ema.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'epoch': epoch,
+                'previous_best': previous_best,
+                'previous_best_ema': previous_best_ema,
+                'best_epoch': best_epoch,
+                'best_epoch_ema': best_epoch_ema
+            }
+            torch.save(checkpoint, os.path.join(args.save_path, 'latest.pth'))
+            if is_best:
+                torch.save(checkpoint, os.path.join(args.save_path, 'best2.pth'))
 if __name__ == '__main__':
     main()
